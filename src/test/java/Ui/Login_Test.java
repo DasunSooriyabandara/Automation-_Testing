d4c52d1c50ab13c7a2b367933d6233afabf7ec5a
Dasun Sooriyabandara <115138972+DasunSooriyabandara@users.noreply.github.com>
2024-02-26 09:25:49 +0530

    test

--- src/test/java/Ui/Login_Test.java
+++ src/test/java/Ui/Login_Test.java
@@ -1,10 +1,16 @@
 package Ui;
+import org.opoenqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Login_Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		WebDriverManager.chromedriver().setup();
+		ChoromeDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.saucedemo.com/");
+		driver.findElement
 	}
 
 }
